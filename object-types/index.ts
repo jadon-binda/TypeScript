1f222173c697520dd9205f3fa4004a0e8369ea99
JB <jadon.binda@gmail.com>
2023-07-02 19:09:09 -0300

    exercise about object types

---
+++ object-types/index.ts
@@ -0,0 +1,10 @@
+// Tipagem de objetos
+let user: {name: string, age: number, job: string, active: boolean, nationality?: string} = {
+    name: "Jadon",
+    age: 30,
+    job: "Web Developer",
+    active: true,
+    //nationality: "Brasileiro"
+}
+
+console.log(user);
