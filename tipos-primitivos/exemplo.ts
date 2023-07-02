23034bcea9aeda2ebfbb53abe21331b49adc16ee
JB <jadon.binda@gmail.com>
2023-07-02 13:18:17 -0300

    exercise about primitive types

---
+++ tipos-primitivos/exemplo.ts
@@ -0,0 +1,14 @@
+let username = "Jadon";
+console.log(typeof username);
+
+let idade = 30;
+console.log(typeof idade);
+
+let eBrasileiro = true;
+console.log(typeof eBrasileiro);
+
+let sobrenome;
+//sobrenome = "Binda";
+//sobrenome = 21;
+//sobrenome = true;
+console.log(typeof sobrenome);
