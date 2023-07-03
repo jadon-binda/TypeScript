9f5828c3e837ab7dcf02fbdd6ce0a4d9907f759b
JB <jadon.binda@gmail.com>
2023-07-03 00:27:48 -0300

    exercise about enum

---
+++ enum/index.ts
@@ -0,0 +1,30 @@
+enum Coordenadas {
+    UP = 1,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+console.log(Coordenadas.LEFT);
+
+enum Resposta {
+    YES,
+    NO
+}
+console.log(Resposta.YES);
+
+enum Transaction {
+    DEBIT = "Você efetuou uma compra no Débito.",
+    CREDIT = "Você efetuou uma compra no Crédito."
+}
+console.log(Transaction.DEBIT);
+
+enum WeekDays {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+console.log(WeekDays)
