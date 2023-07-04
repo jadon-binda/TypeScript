b52e97aff37f57417e5d4ecb6b7b5e1c60fed6d0
JB <jadon.binda@gmail.com>
2023-07-04 11:51:15 -0300

    improve labelPrinting function to receive more than one product

--- label-print/src/index.ts
+++ label-print/src/index.ts
@@ -1,24 +1,41 @@
-const product: {description: string, lot: number, year: number, qty: number} = {
-    description: 'CPU Dual Core 3.0GHZ',
-    lot: 321,
-    year: 2022,
-    qty: 5,
-};
+const productsList: Array<{description: string, lot: number, year: number, qty: number}> = 
+[
+    {
+        description: 'CPU Dual Core 3.0GHZ',
+        lot: 321,
+        year: 2022,
+        qty: 5,
+    },
+    {
+        description: 'RAM Memory',
+        lot: 201,
+        year: 2020,
+        qty: 15,
+    },
+    {
+        description: 'Video Card',
+        lot: 999,
+        year: 2023,
+        qty: 2,
+    },
+];
 
-const labelPrint = (product: {description: string, lot: number, year: number, qty: number}): string[] => {
-    let label: string[] = [];
+const labelPrinting = (products: Array<{description: string, lot: number, year: number, qty: number}>): string[] => {
+    let productLabel: string[] = [];
 
-    for (let i: number = 1; i <= product.qty; i++) {
-        if (i < 10) {
-            label.push(`${product.lot}-${product.year}-00${i}`);
-        } else if (i < 100) {
-            label.push(`${product.lot}-${product.year}-0${i}`);
-        } else {
-            label.push(`${product.lot}-${product.year}-${i}`);
+    products.forEach(product => {        
+        for (let i: number = 1; i <= product.qty; i++) {
+            if (i < 10) {
+                productLabel.push(`${product.lot}-${product.year}-00${i}`);
+            } else if (i < 100) {
+                productLabel.push(`${product.lot}-${product.year}-0${i}`);
+            } else {
+                productLabel.push(`${product.lot}-${product.year}-${i}`);
+            }
         }
-    }
+    });
 
-    return label;
+    return productLabel;
 };
 
-console.log(labelPrint(product));
+console.log(labelPrinting(productsList));
