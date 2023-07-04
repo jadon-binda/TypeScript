9f33752b302f900ee7c03b32fb1911331271aab4
JB <jadon.binda@gmail.com>
2023-07-04 09:05:50 -0300

    function to generate a multiplication table and the display od results

--- multiplication-table/src/index.ts
+++ multiplication-table/src/index.ts
@@ -1,15 +1,15 @@
-const genMultiTable = (listOfNumbers: number[]): string[] => {
-    let multiplicationTable: string[] = [];
+const genMultiTable = (listOfNumbers: number[]): string => {
+    let multiplicationTable: string = '0';
 
     listOfNumbers.forEach(number => {
         for (let i = 0; i <= 10; i++) {
-            multiplicationTable.push(`${number} x ${i} = ${number * i}`);
+            multiplicationTable = `${number} x ${i} = ${number * i}`;
+            console.log(multiplicationTable);            
         }
-        multiplicationTable.push("---------------");
+        console.log("---------------\n");
     });
-
     return multiplicationTable;
 };
 
-const listOfNumbers: number[] = [1, 5, 2, 4];
-console.log(genMultiTable(listOfNumbers));
+const listOfNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+genMultiTable(listOfNumbers);
