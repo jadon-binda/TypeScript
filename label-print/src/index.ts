c3456ca0f21b1204c9498ef16d54d082f0d5df85
JB <jadon.binda@gmail.com>
2023-07-04 11:20:48 -0300

    implement function for label printing

--- label-print/src/index.ts
+++ label-print/src/index.ts
@@ -2,9 +2,17 @@ const product: {description: string, lot: number, year: number, qty: number} = {
     description: 'CPU Dual Core 3.0GHZ',
     lot: 321,
     year: 2022,
-    qty: 5
+    qty: 5,
 };
 
 const labelPrint = (product: {description: string, lot: number, year: number, qty: number}): string[] => {
-    
+    let label: string[] = [];
+
+    for (let i: number = 1; i <= product.qty; i++) {
+        label.push(`${product.lot}-${product.year}-00${i}`);
+    }
+
+    return label;
 };
+
+console.log(labelPrint(product));
