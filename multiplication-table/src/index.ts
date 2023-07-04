8926637c1678ed04742dec5f1fd2be5ddabe221b
JB <jadon.binda@gmail.com>
2023-07-03 23:22:48 -0300

    create an array to store and show the results

--- multiplication-table/src/index.ts
+++ multiplication-table/src/index.ts
@@ -1,6 +1,15 @@
 const genMultiTable = (listOfNumbers: number[]): string[] => {
+    let multiplicationTable: string[] = [];
 
-}
+    listOfNumbers.forEach(number => {
+        for (let i = 0; i <= 10; i++) {
+            multiplicationTable.push(`${number} x ${i} = ${number * i}`);
+        }
+        multiplicationTable.push("---------------");
+    });
+
+    return multiplicationTable;
+};
 
 const listOfNumbers: number[] = [1, 5, 2, 4];
-genMultiTable(listOfNumbers);
+console.log(genMultiTable(listOfNumbers));
