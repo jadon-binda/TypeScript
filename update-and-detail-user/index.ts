6ad73cd82b94d621ad168db31a0dbc421939af56
JB <jadon.binda@gmail.com>
2023-07-08 19:40:27 -0300

    implement function detailUser

--- update-and-detail-user/index.ts
+++ update-and-detail-user/index.ts
@@ -1,9 +1,23 @@
-import {User} from '../register-and-list-users';
+import { User } from '../register-and-list-users';
+import { readFile, writeFile } from '../read-and-write-in-files';
 
 const detailUser = (cpf: string): User => {
 
+    const users = readFile() as User[];
+
+    const foundUser = users.find(user => {
+        return user.cpf === cpf;
+    });   
+    
+    if (!foundUser) {
+        throw new Error('User not found.');
+    }
+
+    return foundUser;
 }
 
-const updateUser = (cpf: string): User => {
+/* const updateUser = (cpf: string): User => {
 
-}
+} */
+
+console.log(detailUser('12345678900'));
