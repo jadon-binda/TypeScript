aa7db7ede9e9e1ffa1c88253b73d48d61e7086d9
JB <jadon.binda@gmail.com>
2023-07-08 20:40:27 -0300

    implemented function updateUser

--- update-and-detail-user/index.ts
+++ update-and-detail-user/index.ts
@@ -2,22 +2,50 @@ import { User } from '../register-and-list-users';
 import { readFile, writeFile } from '../read-and-write-in-files';
 
 const detailUser = (cpf: string): User => {
-
     const users = readFile() as User[];
 
-    const foundUser = users.find(user => {
+    const userFound = users.find(user => {
         return user.cpf === cpf;
     });   
     
-    if (!foundUser) {
+    if (!userFound) {
         throw new Error('User not found.');
     }
 
-    return foundUser;
+    return userFound;
 }
 
-/* const updateUser = (cpf: string): User => {
+const updateUser = (cpf: string, data: User): User => {
+    const users = readFile() as User[];
 
-} */
+    const userFound = users.find(user => {
+        return user.cpf === cpf;
+    });
+
+    if (!userFound) {
+        throw new Error('User not found.');
+    };
+
+    const updatedUser = Object.assign(userFound, data);
+
+    writeFile(users);
+
+    return updatedUser;
+}
 
 console.log(detailUser('12345678900'));
+
+const updateUserData = {
+    name: 'Jadon',
+    email: 'jadon@email.com',
+    cpf: '12345678900',
+    job: 'FullStack Developer',
+    address: {
+        cep: '01234-567',
+        street: 'Rua Imaginaria',
+        complement: '1',
+        district: 'Lourdes',
+        city: 'BH'
+    }
+}
+console.log(updateUser('12345678900', updateUserData));
