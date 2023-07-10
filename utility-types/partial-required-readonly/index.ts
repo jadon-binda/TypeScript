7d68ab3ffd13a29620b6ffe7147b591bcf9d77df
JB <jadon.binda@gmail.com>
2023-07-10 20:17:31 -0300

    examples of Required, Partial and Readonly

--- utility-types/partial-required-readonly/index.ts
+++ utility-types/partial-required-readonly/index.ts
@@ -4,22 +4,46 @@ type User = {
     email: string
 };
 
+// FUNCTION WITHOUT USING UTILITY TYPE
 /* const registerUser = (user: User) => {
     return user;
 }; */
 
+// USING REQUIRED
 /* const registerUser = (user: Required<User>) => {
     return user;
 }; */
 
+// USING PARTIAL
 const registerUser = (user: Partial<User>) => {
     return user;
 };
 
 const person = {
-    // name: 'Zezinho',
+    name: 'Zezinho',
     // email: 'zezin@email.com',
     // age: 25
 };
 
 console.log(registerUser(person));
+
+
+type Car = {
+    brand: string,
+    model: string,
+    year: number
+};
+
+const car = {
+    brand: "Honda",
+    model: "Civic",
+    year: 2000
+};
+
+// USING READONLY
+const registerCar = (car: Readonly<Car>) => {
+    // car.brand = "Toyota";
+    return car;
+};
+
+console.log(registerCar(car));
