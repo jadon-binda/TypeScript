ba187dfc24f599bcec53087645080350c64896d3
JB <jadon.binda@gmail.com>
2023-07-10 22:33:10 -0300

    example of Pick and Omit

--- utility-types/pick-omit/script.ts
+++ utility-types/pick-omit/script.ts
@@ -5,25 +5,25 @@ type People = {
     address: string,
 };
 
+// Pick<Type, Keys>
 const manolo: Pick<People, 'name' | 'email'> = {
     name: 'Manolo',
     email: 'manolito@email.com'
 };
 
+type Keys = 'name' | 'age';
+const luigi: Pick<People, Keys> = {
+    name: 'Luigi',
+    age: 50
+};
+
+// Omit<Type, Keys>
 const antonio: Omit<People, 'address' | 'age'> = {
     name: 'Antonio',
     email: 'tonin@email.com'
 };
 
-type Keys = 'age' | 'email' | 'address';
-
-const mario: Omit<People, Keys> = {
+type Keys2 = 'age' | 'email' | 'address';
+const mario: Omit<People, Keys2> = {
     name: 'Mario'
-};
-
-type Keys2 = 'name' | 'age';
-
-const luigi: Pick<People, Keys2> = {
-    name: 'Luigi',
-    age: 50
 };
