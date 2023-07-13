b6eb7113ed78ae79460901b1a2cb3c285b6e3bf2
JB <jadon.binda@gmail.com>
2023-07-13 12:11:24 -0300

    included Readonly to restrict function return

--- utility-types/exercises/restrict-function-return/scripts.ts
+++ utility-types/exercises/restrict-function-return/scripts.ts
@@ -6,10 +6,25 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+type Connection = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<Connection> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+const dataConn = {
+    username: 'jadon',
+    password: '123',
+    host: 'server',
+    port: '3001',
+    dbname: 'db-shop'
+}
+
+const connect = conexao(dataConn);
