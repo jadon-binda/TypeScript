472fdb808f5266b9ff1211e2b4cef9b3e301d6dc
JB <jadon.binda@gmail.com>
2023-07-15 14:06:07 -0300

    example of Let

--- es6-review/var-let-const/script.js
+++ es6-review/var-let-const/script.js
@@ -2,18 +2,50 @@
 // var - escopo global
 // let e const - escopo local
 
-// var é usado para declarar variáveis, assim como o let
+// var é usado para declarar variáveis
 var nome;
-// é possível declarar uma variável e atribuir o valor depois
+
+// com var, é possível declarar uma variável e atribuir o valor depois
 nome = 'Jadon';
-// uma peculiaridade do var é que ele permite redeclarar uma variável; já o let não
+
+// uma peculiaridade do var é que ele permite redeclarar uma variável, pois pertence ao escopo global
 var nome = 'Samuel';
-// é possível também reatribuir um valor para uma variável criada com var
+
+// é possível reatribuir um valor para uma variável criada com var
 nome = 'Binda';
 
-// como o var pertence ao escopo global, não é feita nenhuma distinção se você utilizá-la dentro de uma estrutura, como uma condicional, por exemplo
+// como o var pertence ao escopo global, não é feita nenhuma distinção se você utilizá-lo dentro de uma estrutura, como uma condicional, por exemplo
 if(true) {
     var nome = 'Brisa';
 }
 
-console.log(nome);
+// console.log(nome);
+
+// *****************************************************************************************************************************************************************************
+
+// let também é usado para declarar variáveis
+let nome1;
+
+// com let, também é possível declarar uma variável e atribuir o valor depois
+nome1 = 'Antonio';
+
+// porém o let não permite redeclarar uma variável dentro do mesmo contexto, por ele pertencer ao escopo local
+// let nome1 = 'Braz';
+
+// também é possível reatribuir um valor para uma variável criada com let
+nome1 = 'Binda';
+
+//console.log(nome1);
+
+// como let pertence ao escopo local, existe diferença em relação a qual contexto em que você o está utilizando
+// no exemplo abaixo, a variável nome1 nunca será lida, pois foi usada dentro de uma estrutura condicional e pertence somente a esse escopo
+if(true) {
+    let nome2 = 'Babua';
+}
+
+// para você manipular a variável criada com let, deve-se criá-la no escopo global ou manipulá-la dentro do próprio contexto onde está sendo usada 
+let nome3;
+if(true) {
+    nome3 = 'Julito';
+}
+console.log(nome3);
